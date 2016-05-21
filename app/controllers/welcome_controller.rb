46b71525fae30abd9832a9b387c5395acf44d224
olegkonopka <godschildknopik@gmail.com>
2016-05-21 21:14:09 +0300

    Edit Welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-  	if signed_in?
-  		@user = User.find(current_user)
-		@task = Task.new
-	  	@tasks = @user.tasks
-	  	@tasksAll = Task.all
-	  end
+
   end
 end
